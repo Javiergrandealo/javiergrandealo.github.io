db04a2a1c5f786a87973325962a34146ed39d3af
Javier Grande Alonso <franciscojaviergrandealonso@gmail.com>
2025-04-25 14:31:13 +0200

    prueba hecha por el copi

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
